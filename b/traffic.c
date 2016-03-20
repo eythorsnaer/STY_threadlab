78d2f3cefef87ae78fe54cb05f2ec4087385c1c1
eythorst14 <eythorst14@skel.ru.is>
2016-03-20 17:57:29 +0000

    æ æ æ

--- b/traffic.c
+++ b/traffic.c
@@ -9,14 +9,22 @@ int num_pedestrians;
 void *vehicles(void *arg);
 void *pedestrians(void *arg);
 
-sem_t thread_mutex;
+sem_t mutex;
+sem_t vhLock;
+sem_t vvLock;
+sem_t phLock;
+sem_t pvLock;
 
 pthread_t *pedestrian_thread;
 pthread_t *vehicle_thread;
 
 void init()
 {
-    sem_init(&thread_mutex, 0, 1);
+    sem_init(&mutex, 0, 1);
+    sem_init(&vhLock, 0, 1);
+    sem_init(&vvLock, 0, 1);
+    sem_init(&phLock, 0, 1);
+    sem_init(&pvLock, 0, 1);
 
     pedestrian_thread = malloc(sizeof(pthread_t) * num_pedestrians);
     vehicle_thread = malloc(sizeof(pthread_t) * num_vehicles);
@@ -25,7 +33,7 @@ void init()
 void spawn_pedestrian(thread_info *arg)
 {
     Pthread_create(&pedestrian_thread[arg->thread_nr], 0, pedestrians, arg);
-
+    
 }
 
 void spawn_vehicle(thread_info *arg)
@@ -40,11 +48,28 @@ void *vehicles(void *arg)
     // Each thread needs to call these functions in this order
     // Note that the calls can also be made in helper functions
     int place = vehicle_arrive(info);
-    P(&thread_mutex);
-    vehicle_drive(info);
-    vehicle_leave(info);
-    V(&thread_mutex);
+    
+    //P(&mutex);
 
+    if (arg->crossing == 1)
+    {
+	P(&vhLock);
+	P(&pvLock);
+	V(&phLock);
+	
+	vehicle_drive(info);
+	vehicle_leave(info);
+   
+    }
+    else if(arg->crossing == 3)
+    {
+	P(&vvLock);
+	P(&phLock);
+	V(&pvLock);
+    }
+       
+    V(&mutex);
+    
     return NULL;
 }
 
@@ -55,10 +80,26 @@ void *pedestrians(void *arg)
     // Each thread needs to call these functions in this order
     // Note that the calls can also be made in helper functions
     int place = pedestrian_arrive(info);
-    P(&thread_mutex);
+
+    P(&mutex);
+
+    if (arg->crossing == 0)
+	{
+	    P(&vvLock);
+	    V(&phLock);
+	}
+    else if(arg->crossing == 2)
+	{
+	    P(&vvLock);
+	    P(&phLock);
+	    V(&pvLock);
+	}
+
+
     pedestrian_walk(info);
     pedestrian_leave(info);
-    V(&thread_mutex);
+
+    V(&mutex);
 
     return NULL;
 }
