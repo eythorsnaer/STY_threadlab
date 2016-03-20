34c0780f6589695590b93669f38dd8c5b5f31581
eythorst14 <eythorst14@skel.ru.is>
2016-03-20 17:05:46 +0000

    remove some file :p

--- a/traffic.c
+++ a/traffic.c
@@ -6,43 +6,67 @@ int K;
 int num_vehicles;
 int num_pedestrians;
 
+void *vehicles(void *arg);
+void *pedestrians(void *arg);
+
+sem_t thread_mutex;
+sem_t pMutex;
+sem_t vMutex;
+sem_t plock;
+sem_t vlock;
+
+pthread_t *pedestrian_thread;
+pthread_t *vehicle_thread;
+
 void init()
 {
+    sem_init(&thread_mutex, 0, 1);
 
+    pedestrian_thread = malloc(sizeof(pthread_t) * num_pedestrians);
+    vehicle_thread = malloc(sizeof(pthread_t) * num_vehicles);
 }
 
 void spawn_pedestrian(thread_info *arg)
 {
+    //pedestrians((void *) arg);
+    Pthread_create(&pedestrian_thread[arg->thread_nr], 0, pedestrians, arg);
 
 }
   
 void spawn_vehicle(thread_info *arg)
 {
+    //vehicles((void *) arg);
+    Pthread_create(&vehicle_thread[arg->thread_nr], 0, vehicles, arg);
 
 }
 
 void *vehicles(void *arg)
 {
     thread_info *info = arg;
+    int vCounter = 0;
 
     // Each thread needs to call these functions in this order
     // Note that the calls can also be made in helper functions
     int place = vehicle_arrive(info);
+    P(&thread_mutex);
     vehicle_drive(info);
-    vehicle_leave(info);
-
+    vehicle_leave(info);    
+    V(&thread_mutex);
+        
     return NULL;
 }
 
 void *pedestrians(void *arg)
 {
     thread_info *info = arg;
-
-    // Each thread needs to call these functions in this order
+    int pCounter = 0;
+    // Each  thread needs to call these functions in this order
     // Note that the calls can also be made in helper functions
     int place = pedestrian_arrive(info);
-    pedestrian_walk(info);
+    P(&thread_mutex);
+    pedestrian_walk(info);    
     pedestrian_leave(info);
+    V(&thread_mutex);
 
     return NULL;
 }
@@ -50,5 +74,10 @@ void *pedestrians(void *arg)
 
 void clean()
 {
-
+    for(int i = 0; i < num_pedestrians; i++) {
+	Pthread_join(pedestrian_thread[i], NULL);
+    }
+    for(int i = 0; i < num_vehicles; i++) {
+	Pthread_join(vehicle_thread[i], NULL);
+    }			    
 }
--- b/traffic.c
+++ b/traffic.c
@@ -6,19 +6,31 @@ int K;
 int num_vehicles;
 int num_pedestrians;
 
+void *vehicles(void *arg);
+void *pedestrians(void *arg);
+
+sem_t thread_mutex;
+
+pthread_t *pedestrian_thread;
+pthread_t *vehicle_thread;
+
 void init()
 {
+    sem_init(&thread_mutex, 0, 1);
 
+    pedestrian_thread = malloc(sizeof(pthread_t) * num_pedestrians);
+    vehicle_thread = malloc(sizeof(pthread_t) * num_vehicles);
 }
 
 void spawn_pedestrian(thread_info *arg)
 {
+    Pthread_create(&pedestrian_thread[arg->thread_nr], 0, pedestrians, arg);
 
 }
 
 void spawn_vehicle(thread_info *arg)
 {
-
+    Pthread_create(&vehicle_thread[arg->thread_nr], 0, vehicles, arg);
 }
 
 void *vehicles(void *arg)
@@ -28,8 +40,10 @@ void *vehicles(void *arg)
     // Each thread needs to call these functions in this order
     // Note that the calls can also be made in helper functions
     int place = vehicle_arrive(info);
+    P(&thread_mutex);
     vehicle_drive(info);
     vehicle_leave(info);
+    V(&thread_mutex);
 
     return NULL;
 }
@@ -41,8 +55,10 @@ void *pedestrians(void *arg)
     // Each thread needs to call these functions in this order
     // Note that the calls can also be made in helper functions
     int place = pedestrian_arrive(info);
+    P(&thread_mutex);
     pedestrian_walk(info);
     pedestrian_leave(info);
+    V(&thread_mutex);
 
     return NULL;
 }
@@ -50,5 +66,10 @@ void *pedestrians(void *arg)
 
 void clean()
 {
-
+    for(int i = 0; i < num_pedestrians; i++) {
+	Pthread_join(pedestrian_thread[i], NULL);
+    }
+    for(int i = 0; i < num_vehicles; i++) {
+	Pthread_join(vehicle_thread[i], NULL);
+    }
 }
